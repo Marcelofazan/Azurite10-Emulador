67920e872c0702eefab8e5437acfd2a6880e2e80
Marcelofazan <56393266+Marcelofazan@users.noreply.github.com>
2026-06-10 13:05:37 -0300

    Add BlobClientOptions to BlobContainerClient initialization

--- EmuladorAzurite/BlobService.cs
+++ EmuladorAzurite/BlobService.cs
@@ -1,4 +1,4 @@
-﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,12 +11,17 @@ namespace EmuladorAzurite
         {
             var connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
 
-            var client = new BlobContainerClient(connectionString, "images");
-            var stream = await new HttpClient().GetStreamAsync("https://www.nopcommerce.com/images/thumbs/0024674_azure-blob-storage.png");
+            var options = new BlobClientOptions(BlobClientOptions.ServiceVersion.V2025_11_05);
+            options.Diagnostics.IsLoggingEnabled = false;
+
+            var client = new BlobContainerClient(connectionString, "images", options);
+
+            using var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync("https://www.nopcommerce.com/images/thumbs/0024674_azure-blob-storage.png");
 
             var blob = client.GetBlobClient("AzureBlobStorage.png");
-            await blob.UploadAsync(stream);
 
+            await blob.UploadAsync(stream, overwrite: true);
             Console.WriteLine($"Image {blob.Name} inserido com sucesso!");
 
         }
